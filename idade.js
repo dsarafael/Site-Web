9dd9e98442b1a4644172c97db818f6016371ceae
Rafael Araújo <131030677+dsarafael@users.noreply.github.com>
2024-06-04 18:51:11 -0300

    a

--- idade.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var isAdult = confirm("Você tem 18 anos ou mais?");
-    if (!isAdult) {
-        window.location.href = "https://www.google.com";
-    }
-});
